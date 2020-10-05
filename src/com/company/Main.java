e5cfb6fdf6a941e561013b13c532babd6abdc39b
Merwan Laouini <merwanlaouini@gmail.com>
2020-10-05 16:57:22 +0200

    OUE le commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,24 +2,51 @@ package com.company;
 
 public class Main {
 
+//    static void afficherscore(int scores[]){
+//        for (int i=0;i< scores.length;i++){
+//            System.out.println(scores[i]);
+//        }
+//    }
+//
+//    static int plusgrosscore(int scores[]){
+//        int highS = 0;
+//        for (int i = 0; i< scores.length; i++){
+//            if (scores[i]>highS){
+//                highS=scores[i];
+//
+//            }
+//        }
+//        return highS;
+//    }
+
+    static boolean supp10(int scores[]){
+        int a=10;
+        boolean supp10 = false;
+        for (int i=0; i<scores.length; i++){
+            if (scores[i]>=a){
+                supp10= true;
+                System.out.println(scores[i] + " est supp   a 10");
+            }
+        }
+        return supp10;
+    }
+
     public static void main(String[] args) {
 	// write your code here
-        int scores[]= {24,32,42,52,62,72,82};
+        int scores[]= {4,32,42,52,692,72,82,};
         int moyenne = 0;
         int total= scores.length;
-        for (int i= 0; i<scores.length; i++){
-            int s = scores[i];
-            System.out.println(scores[i]);
-
-        }
-
         for (int i = 0; i< scores.length; i++){
             moyenne= moyenne + scores[i];
 
         }
-        System.out.println(moyenne/total);
+//        System.out.println(moyenne/total);
+//        afficherscore(scores);
+        supp10(scores);
+        //System.out.println(plusgrosscore(scores));
 
 
 
     }
+
 }
