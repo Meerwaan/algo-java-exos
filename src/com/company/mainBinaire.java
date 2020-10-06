28624d964d2f92dc3c992d7e37a379ae385130ae
Merwan Laouini <merwanlaouini@gmail.com>
2020-10-06 14:25:01 +0200

    tp4 Check

---
+++ src/com/company/mainBinaire.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class mainBinaire {
+    /**
+     * fonction de recherche binaire
+     * @param sortedArray index du tableau
+     * @param seacrh 
+     * @return
+     */
+    public static int tunBinarySeacrh(int sortedArray[],int seacrh){
+        int index = -1;
+        int low = 0;
+        int max = sortedArray.length-1;
+        while (low<=max){
+            int mid = (low+max)/2;
+            if (sortedArray[mid]<seacrh){
+                low= mid-1;
+            }else if (sortedArray[mid]>seacrh){
+                max= mid-1;
+            }else if (sortedArray[mid]==seacrh){
+                index= mid;
+                break;
+            }
+        }return index;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        int scores[]= {1,2,3,4,5,6,7,8,9};
+        System.out.println(tunBinarySeacrh(scores, 7));
+
+
+
+
+
+
+
+
+    }
+}
